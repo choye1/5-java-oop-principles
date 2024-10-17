27e2416f917eb7fa98580761da6470e808894679
choye1 <makcim233237@gmail.com>
2024-10-17 15:02:40 +0500

    Скоробогатов ПрИ-202 task1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -3,5 +3,8 @@ package com.example.task01;
 public class Task01Main {
     public static void main(String[] args) {
 
+        Logger logger1 = Logger.getLogger("test");
+        Logger logger2 = Logger.getLogger("test");
+        System.out.println(logger2 == logger1);
     }
 }
